2ef3fecb5a44fdfaa42c86149a3f9ecb981a59a6
Moh Albaghli <30597715+dolphini13@users.noreply.github.com>
2021-10-05 17:03:31 +1100

    Revert "testing husky"
    This reverts commit e4d2c387b19985f00d5b309fc42b96a9962cfb11.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return <div className="App">Hello 
-  World</div>;
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
 }
 
 export default App;
