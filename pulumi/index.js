bd5dcd3435fa53124ec4741974ae073f2397a5bf
Moh Albaghli <30597715+dolphini13@users.noreply.github.com>
2021-10-06 11:40:02 +1100

    feat: build a bucket via pulumi code on s3 with configuration

--- pulumi/index.js
+++ pulumi/index.js
@@ -1,21 +1,59 @@
 "use strict";
 const aws = require("@pulumi/aws");
 
-// Create an AWS resource (S3 Bucket)
-const SkandPulumi = new aws.s3.Bucket(
-  "skand-pulumi",
-  {
-    acl: "private",
-    bucket: "skand-codepipline-demo",
-    forceDestroy: false,
-    website: {
-      indexDocument: "index.html",
-    },
+// Create a new S3 Bucket
+const bucket = new aws.s3.Bucket("skand-pulumi", {
+  website: {
+    indexDocument: "index.html",
   },
-  {
-    protect: true,
-  }
+});
+
+// get origin access identification for s3Distribution later on
+const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(
+  "skand-pulumi"
 );
 
+const s3Distribution = new aws.cloudfront.Distribution("s3Distribution", {
+  origins: [
+    {
+      domainName: bucket.bucketRegionalDomainName,
+      originId: bucket.arn,
+      s3OriginConfig: {
+        originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath,
+      },
+    },
+  ],
+  enabled: true,
+  isIpv6Enabled: true,
+  defaultRootObject: "index.html",
+  defaultCacheBehavior: {
+    allowedMethods: ["GET", "HEAD"],
+    cachedMethods: ["GET", "HEAD"],
+    targetOriginId: bucket.arn,
+    forwardedValues: {
+      queryString: false,
+      cookies: {
+        forward: "none",
+      },
+    },
+    viewerProtocolPolicy: "allow-all",
+    minTtl: 0,
+    defaultTtl: 3600,
+    maxTtl: 86400,
+  },
+  priceClass: "PriceClass_All",
+  restrictions: {
+    geoRestriction: {
+      restrictionType: "none",
+    },
+  },
+  viewerCertificate: {
+    cloudfrontDefaultCertificate: true,
+  },
+});
+
 // Export the name of the bucket
-exports.bucketName = SkandPulumi.id;
+exports.bucketName = bucket.id;
+exports.bucket = bucket;
+exports.originAccessIdentity = originAccessIdentity;
+exports.cloudfrontDistribution = s3Distribution;
